64079c0d71c1f33ff4279f1d067167fcd1748993
vegeta <phyala@gmail.com>
2019-09-17 20:28:38 +0800

    Dynamic document

--- lib/super_pdf/document.rb
+++
@@ -1,9 +0,0 @@
-require 'prawn'
-require 'prawn/table'
-
-module SuperPDF
-  class Document < ::Prawn::Document
-    attr_accessor :default_font_family
-    include Renderer
-  end
-end
--- lib/super_pdf/enhance.rb
+++ lib/super_pdf/enhance.rb
@@ -1,4 +1,6 @@
-require "super_pdf/document"
+require 'prawn'
+require 'prawn/table'
+require 'prawn/icon'
 
 module SuperPDF
   module Enhance
@@ -9,13 +11,22 @@ module SuperPDF
       include Page
 
       def pdf
-        @pdf ||= Document.new(
+        @pdf ||= document_klass.new(
           page_size: page_size,
           skip_page_creation: true,
           margin: 0
         ).tap do |document|
           document.font_families.update(Font::FAMILIES) unless Font::FAMILIES.empty?
           document.default_font_family = default_font_family
+        end
+      end
+
+      protected
+
+      def document_klass
+        @document_klass ||= Class.new(::Prawn::Document) do
+          attr_accessor :default_font_family
+          include Renderer
         end
       end
     end
